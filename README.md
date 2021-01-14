53011056f9586f95ae3a51581610ba6394d42042
liuhao1088 <1350569467@qq.com>
2021-01-14 18:34:55 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nationwideUnproforProject
-账号:
+账号:fnck_lhthirteen@163.com
---
+++ pages/inactivated/inactivated.js
@@ -0,0 +1,126 @@
+// pages/inactivated/inactivated.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    checked:false,
+    img:[]
+  },
+
+  handleChange(e) {
+    console.log(e)
+    let value = e.detail.value;
+    if(value === 'true'){
+      
+    }
+    
+  },
+   //弹窗
+   showModal(e) {
+    let that = this;
+    let target = e.currentTarget.dataset.target;
+      that.setData({
+        modalName: target
+      })
+  },
+  hideModal(e) {
+    this.setData({
+      modalName: null,
+      checked:false
+    })
+  },
+
+  ChooseImage() {
+    wx.chooseImage({
+      count: 1, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        this.setData({
+          img: res.tempFilePaths
+        })
+      }
+    });
+  },
+
+  ViewImage(e) {
+    wx.previewImage({
+      urls: this.data.img,
+      current: e.currentTarget.dataset.url
+    });
+  },
+
+  DelImg(e) {
+    let img = this.data.img;
+    wx.showModal({
+      content: '确定要删除图片吗？',
+      cancelText: '取消',
+      confirmText: '确定',
+      success: res => {
+        if (res.confirm) {
+          img.splice(0, 1);
+          this.setData({
+            img
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
